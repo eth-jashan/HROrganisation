e81e7653c59575909fd50aa634ebf9de162d7204
Piyush <piyushjb7@hotmail.com>
2021-03-03 15:58:12 +0530

    new files

---
+++ navigator/Navigator.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
